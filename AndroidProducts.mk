049b4147b5ef6ee86472b7294196eb90b888ad60
prateekpunetha <prateekpunetha@gmail.com>
2020-05-15 02:11:45 +0530

    products: Android.mk: Cleanup Remove other devices because It's for vince and other device doesn't make any sense
    Signed-off-by: prateekpunetha <prateekpunetha@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,46 +15,8 @@
 
 # CAF
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/beryllium/pa_beryllium.mk \
-    $(LOCAL_DIR)/cepheus/pa_cepheus.mk \
-    $(LOCAL_DIR)/davinci/pa_davinci.mk \
-    $(LOCAL_DIR)/dipper/pa_dipper.mk \
-    $(LOCAL_DIR)/gemini/pa_gemini.mk \
-    $(LOCAL_DIR)/mata/pa_mata.mk \
-    $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
-    $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
-    $(LOCAL_DIR)/oneplus7pro/pa_oneplus7pro.mk \
-    $(LOCAL_DIR)/polaris/pa_polaris.mk \
-    $(LOCAL_DIR)/sagit/pa_sagit.mk \
     $(LOCAL_DIR)/vince/pa_vince.mk
-# SONY OpenDevices
-# Loire 8956
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/kugo/pa_kugo.mk \
-    $(LOCAL_DIR)/suzu/pa_suzu.mk
-
-# Tone 8996
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/dora/pa_dora.mk \
-    $(LOCAL_DIR)/kagura/pa_kagura.mk \
-    $(LOCAL_DIR)/keyaki/pa_keyaki.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
-    pa_beryllium-userdebug \
-    pa_cepheus-userdebug \
-    pa_davinci-userdebug \
-    pa_dipper-userdebug \
-    pa_dora-userdebug \
-    pa_gemini-userdebug \
-    pa_kagura-userdebug \
-    pa_keyaki-userdebug \
-    pa_kugo-userdebug \
-    pa_mata-userdebug \
-    pa_oneplus3-userdebug \
-    pa_oneplus6-userdebug \
-    pa_oneplus7pro-userdebug \
-    pa_polaris-userdebug \
-    pa_sagit-userdebug \
-    pa_suzu-userdebug \
     pa_vince-userdebug
